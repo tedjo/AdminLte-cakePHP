97476cd47c1601d652f006af76c94c592496ca0f
tedjo <tedjowell@gmail.com>
2020-01-25 08:41:39 +0700

    master AdminLte

---
+++ config/adminlte.php
@@ -0,0 +1,20 @@
+return [
+'Theme' => [
+'title' => 'AdminLTE',
+'logo' => [
+'mini' => '<b>A</b>LT',
+'large' => '<b>Admin</b>LTE'
+],
+'login' => [
+'show_remember' => true,
+'show_register' => true,
+'show_social' => true
+],
+'folder' => ROOT,
+'skin' => 'blue'
+]
+];
+
+// config/bootstrap.php
+
+Configure::load('adminlte', 'default');
